025338dd5e182680ac0efe8a49f75c80ed8a8c55
otgonjargal2019 <50820049+otgonjargal2019@users.noreply.github.com>
2022-09-05 16:12:00 +0800

    zasvar

--- src/components/Table/style.ts
+++ src/components/Table/style.ts
@@ -4,12 +4,16 @@ const Table = styled.table`
   border-collapse: separate;
   border-spacing: 0;
   color: #333;
-  width: 50%;
+  width: 700px;
   font-size: 14px;
   font-weight: 500;
   margin-bottom: 12px;
   border-left: 1px solid #d6d2ce;
   border-bottom: 1px solid #000;
+
+  display: block;
+  overflow-x: auto;
+  white-space: nowrap;
 
   thead {
     tr {
