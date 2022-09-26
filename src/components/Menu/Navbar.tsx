f490c1eac2fa3a1af883ebb89c969fc7cc2cc3b3
otgonjargal2019 <50820049+otgonjargal2019@users.noreply.github.com>
2022-09-26 09:06:15 +0800

    zasvar

--- src/components/Menu/Navbar.tsx
+++ src/components/Menu/Navbar.tsx
@@ -10,13 +10,12 @@ const Navbar = (props: INabar) => {
   let list: any;
   if (props.data) {
     const clone = JSON.parse(JSON.stringify(props.data));
-    list = getNestedChildren(clone, null, "menuId", "parentMenuId");
+    list = getNestedChildren(clone, "", "menuId", "parentMenuId");
   }
 
   return (
     <nav>
       <ul className="menus">
-        {" "}
         {list.map((menu: any, index: number) => {
           const depthLevel = 0;
           return <MenuItems items={menu} key={index} depthLevel={depthLevel} />;
