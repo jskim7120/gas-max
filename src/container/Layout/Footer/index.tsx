10692819854dc5f3285d225e20141a87e7f99c63
Sugarbayar Dorj <dorjsugaraaz@gmail.com>
2022-10-03 13:24:35 +0800

    added footer side

--- src/container/Layout/Footer/index.tsx
+++ src/container/Layout/Footer/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { FooterContainer } from "./style";
+import {
+  FooterContainer,
+  AddrContainer,
+  AddrDetail,
+  LogoImgContainer,
+  SearchResultCont,
+  SearchResult,
+  SearchResultItem,
+  SearchResultTitle,
+  SearchResultData,
+} from "./style";
 import PartnerImg from "assets/image/company_partners.png";
 import { SearchIcon } from "components/AllSvgIcon";
 import Hamburger from "assets/image/footer-humburger.png";
+import TLogo from "assets/image/TLogo.png";
+import GLogo from "assets/image/GLogo.png";
+import BarCodeLogo from "assets/image/BarCodeLogo.png";
 
 function Footer() {
   return (
@@ -19,6 +32,54 @@ function Footer() {
           </button>
         </form>
       </div>
+      <AddrContainer>
+        <AddrDetail>
+          <p>
+            000-00000 가나라다라바사아자차카...<span>[중지]</span>
+          </p>
+          <p>010-2701-1234 010-2079-5390</p>
+          <p>서울시 광진구 자양동 584-3</p>
+        </AddrDetail>
+        <LogoImgContainer>
+          <img src={TLogo} />
+          <img src={GLogo} />
+          <img src={BarCodeLogo} />
+        </LogoImgContainer>
+      </AddrContainer>
+      <SearchResultCont>
+        <SearchResult>
+          <SearchResultItem>
+            <SearchResultTitle>수금방법</SearchResultTitle>
+            <SearchResultData>카드결제</SearchResultData>
+          </SearchResultItem>
+          <SearchResultItem>
+            <SearchResultTitle>가상계좌</SearchResultTitle>
+            <SearchResultData>1234-2345-1234-9876</SearchResultData>
+          </SearchResultItem>
+        </SearchResult>
+        <SearchResult>
+          <SearchResultItem>
+            <SearchResultTitle>계약일</SearchResultTitle>
+            <SearchResultData>2022-09-20</SearchResultData>
+          </SearchResultItem>
+          <SearchResultItem>
+            <SearchResultTitle>점검일</SearchResultTitle>
+            <SearchResultData>2023-01-20</SearchResultData>
+          </SearchResultItem>
+        </SearchResult>
+        <SearchResult>
+          <SearchResultItem>
+            <SearchResultTitle>보증금</SearchResultTitle>
+            <SearchResultData>10,000,000</SearchResultData>
+          </SearchResultItem>
+          <SearchResultItem>
+            <SearchResultTitle style={{ backgroundColor: "#CC15A2" }}>
+              미수금
+            </SearchResultTitle>
+            <SearchResultData>139,000</SearchResultData>
+          </SearchResultItem>
+        </SearchResult>
+      </SearchResultCont>
       <div>
         <img src={Hamburger} />
       </div>
--- src/container/Layout/Footer/style.ts
+++ src/container/Layout/Footer/style.ts
@@ -29,8 +29,8 @@ export const FooterContainer = styled.div`
   }
 
   .container {
-    padding-left: 11px;
-    padding-top: 23px;
+    // padding-left: 11px;
+    // padding-top: 23px;
   }
 
   form {
@@ -66,6 +66,8 @@ export const FooterContainer = styled.div`
     cursor: pointer;
     height: 21px;
     background: #ff0000;
+    padding-top: 3px;
+    padding-left: 1px;
   }
 
   button[type="submit"]:hover {
@@ -92,4 +94,97 @@ export const FooterContainer = styled.div`
       url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E")
       no-repeat 13px center;
   }
+`;
+
+export const AddrContainer = styled.div`
+  display: flex;
+  width: 420px;
+  justify-content: space-between;
+  margin-top: -3px;
+`;
+
+export const LogoImgContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 77px;
+  height: 22px;
+  align-items: center;
+  img {
+    display: block;
+  }
+  img:first-child {
+    width: 20px;
+    height: 20px;
+  }
+  img:nth-child(2) {
+    width: 22px;
+    height: 20px;
+  }
+  img:nth-child(3) {
+    width: 17px;
+    height: 16px;
+  }
+`;
+
+export const AddrDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 60px;
+  font-family: "Noto Sans KR";
+  p:first-child {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 5px;
+    span {
+      color: #f90606;
+    }
+  }
+  p:nth-child(2) {
+    font-size: 14px;
+    font-weight: 400;
+    color: #3440e6;
+  }
+  p:nth-child(3) {
+    font-size: 14px;
+    font-weight: 400;
+    color: #000;
+  }
+`;
+
+export const SearchResultCont = styled.div`
+  width: 765px;
+  display: flex;
+  justify-content: space-between;
+  font-family: "Segoe UI", Arial, sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+`;
+export const SearchResult = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 211px;
+  grid-gap: 6px;
+`;
+export const SearchResultItem = styled.div`
+  display: flex;
+  grid-gap: 4px;
+`;
+export const SearchResultTitle = styled.div`
+  width: 63px;
+  height: 28px;
+  border: 1px solid #707070;
+  background: #707070;
+  text-align: center;
+  color: #f6f6f6;
+  line-height: 24px;
+`;
+export const SearchResultData = styled.div`
+  width: 144px;
+  height: 28px;
+  border: 1px solid #707070;
+  background: #f6efef;
+  text-align: center;
+  color: #0057aa;
+  line-height: 24px;
 `;
