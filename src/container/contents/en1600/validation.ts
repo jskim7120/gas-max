4346b8d629635a4826277169aecdb15809e864ed
Sugarbayar Dorj <dorjsugaraaz@gmail.com>
2022-11-01 16:59:36 +0800

    fixed en1100 areacode

--- src/container/contents/en1600/validation.ts
+++ src/container/contents/en1600/validation.ts
@@ -5,7 +5,7 @@ export const schema = yup.object({
     .string()
     .typeError("이 필드는 필수 항목입니다")
     .matches(/[0-9]+/gi, "숫자만 입력")
-    .length(2, "3자리 숫자만  입력"),
+    .length(2, "2자리 숫자만  입력"),
   areaCode: yup.string(),
   swPaykum: yup.number().nullable(true).typeError("숫자만 입력"),
 });
