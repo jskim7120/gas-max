23874025bc07c775fd74c9798931280c3643518a
Sugarbayar Dorj <dorjsugaraaz@gmail.com>
2022-11-07 16:31:29 +0800

    validation and swName required

--- src/container/contents/en1400/validation.ts
+++ src/container/contents/en1400/validation.ts
@@ -9,18 +9,18 @@ export const schema = yup.object({
     .typeError("이 필드는 필수 항목입니다")
     .matches(/[0-9]+/gi, "숫자만 입력")
     .length(3, "3자리 숫자만  입력"),
-  bpIndanga: yup
-    .number()
-    .typeError("숫자만 입력")
-    .transform((_, val) => {
-      return val != "" ? (reg.test(val) ? parseInt(val) : "string") : 0;
-    })
-    .nullable(true),
-  bpOutdanga: yup
-    .number()
-    .typeError("숫자만 입력")
-    .transform((_, val) => {
-      return val != "" ? (reg.test(val) ? parseInt(val) : "string") : 0;
-    })
-    .nullable(true),
+  // bpIndanga: yup
+  //   .number()
+  //   .typeError("숫자만 입력")
+  //   .transform((_, val) => {
+  //     return val != "" ? (reg.test(val) ? parseInt(val) : "string") : 0;
+  //   })
+  //   .nullable(true),
+  // bpOutdanga: yup
+  //   .number()
+  //   .typeError("숫자만 입력")
+  //   .transform((_, val) => {
+  //     return val != "" ? (reg.test(val) ? parseInt(val) : "string") : 0;
+  //   })
+  //   .nullable(true),
 });
