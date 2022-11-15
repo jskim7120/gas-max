072c74e57a19d12151b25c73897b623fc42080c1
Sugarbayar Dorj <dorjsugaraaz@gmail.com>
2022-11-15 15:57:46 +0800

    created cm1300\

--- src/components/tab/tabContent.tsx
+++ src/components/tab/tabContent.tsx
@@ -9,6 +9,7 @@ import EN1800 from "container/contents/en1800";
 import EN1900 from "container/contents/en1900";
 import EN2000 from "container/contents/en2000";
 import CM1100 from "container/contents/cm1100";
+import CM1300 from "container/contents/cm1300";
 
 export const getContent = (id: any, depthFullName: string) => {
   switch (id) {
@@ -36,5 +37,7 @@ export const getContent = (id: any, depthFullName: string) => {
       return <EN2000 depthFullName={depthFullName} menuId={id} />;
     case "CM1100":
       return <CM1100 depthFullName={depthFullName} menuId={id} />;
+    case "CM1300":
+      return <CM1300 depthFullName={depthFullName} menuId={id} />;
   }
 };
---
+++ src/container/contents/cm1300/model.ts
@@ -0,0 +1,29 @@
+export interface ICM1300 {
+  areaCode: string;
+  aptCode: string;
+  aptName: string;
+  aptTypeName: string;
+  aptF: number;
+  aptS: number;
+  aptSum: number;
+  swName: string;
+  aptType: string;
+  aptZipcode: string;
+  aptAddr1: string;
+  aptAddr2: string;
+  apt4ho: string;
+  apt4f: string;
+  aptBf: string;
+  aptSwCode: string;
+  aptJyCode: string;
+  aptCustgubun: string;
+  aptRh2o: string;
+  aptRdangaType: string;
+  aptAnkum: string;
+  aptSisulkum: string;
+  aptRdangaSign: string;
+  aptMeterkum: string;
+  aptPer: string;
+  aptGumdate: string;
+  aptSukumtype: string;
+}
