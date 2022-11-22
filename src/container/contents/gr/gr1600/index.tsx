3907485b90a450eafa388bef113c9fc0de40c40a
otgonjargal2019 <50820049+otgonjargal2019@users.noreply.github.com>
2022-11-22 09:33:39 +0800

    zasvar

--- src/container/contents/gr/gr1600/index.tsx
+++ src/container/contents/gr/gr1600/index.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
-import { DetailHeader, DetailWrapper, Wrapper } from "../style";
+import React, { useEffect, useState } from "react";
+import API from "app/axios";
+import { useGetCommonDictionaryQuery } from "app/api/commonDictionary";
+import { GR1600SEARCH } from "app/path";
+import { DetailHeader, DetailWrapper, TableWrapper, Wrapper } from "../style";
 import DataGridFooter from "components/dataGridFooter/dataGridFooter";
 import Grid from "./grid1";
+
 function GR1600({
   depthFullName,
   menuId,
@@ -10,13 +14,34 @@ function GR1600({
   menuId: string;
 }) {
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const { data: dataCommonDic } = useGetCommonDictionaryQuery({
+    groupId: "GR",
+    functionName: "GR1600",
+  });
+
+  console.log("dataCommonDic:", dataCommonDic);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await API.get(GR1600SEARCH);
+      console.log("data::::", data);
+      setData(data);
+    } catch (error) {
+      console.log("GR1600 DATA fetch error =======>", error);
+    }
+  };
   return (
     <>
       <DetailHeader>
         <p>{depthFullName}</p>
       </DetailHeader>
       <Wrapper>
-        {/* <Grid /> */}
+        <TableWrapper>{/* <Grid /> */}</TableWrapper>
         <DetailWrapper></DetailWrapper>
       </Wrapper>
       <DataGridFooter dataLength={data?.length > 0 ? data.length : 0} />
