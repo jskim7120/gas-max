46d20c8ac3360849c63e8f7e1fdb05c34993d9c4
otgonjargal2019 <50820049+otgonjargal2019@users.noreply.github.com>
2022-11-22 10:37:03 +0800

    zasvar

--- src/container/contents/gr/style.tsx
+++ src/container/contents/gr/style.tsx
@@ -7,26 +7,41 @@ export const DetailHeader = styled.div`
   align-items: center;
   padding: 0 6px 0 15px;
   border-bottom: 5px solid #707070;
-  p {
-    color: #0a0a0a;
-    font-family: "SegoeUI";
-    font-size: 12px;
-  }
 `;
 
 export const Wrapper = styled.div`
   display: flex;
   gap: 0px;
-  height: calc(100% - 40px);
+  height: calc(100% - 10px);
   border: 1px solid red;
 `;
 
 export const LeftSection = styled.div<{ width?: string }>`
   width: ${(props) => (props.width ? props.width : "50%")};
-  border-right: 3px solid #707070;
+  // border-right: 3px solid #707070;
+
+  .top-header {
+    background-color: #dbdbdb;
+    height: 35px;
+    border-bottom: 5px solid #707070;
+    display: flex;
+    align-items: center;
+
+    p {
+      color: #0a0a0a;
+      font-family: "SegoeUI";
+      font-size: 12px;
+    }
+  }
 `;
 
 export const RightSection = styled.div<{ width?: string }>`
   width: ${(props) => (props.width ? props.width : "50%")};
   overflow-y: auto;
+
+  .top-header {
+    background-color: #dbdbdb;
+    height: 35px;
+    border-bottom: 5px solid #707070;
+  }
 `;
