b6847e0d152d9788aba39ee499d394d236ed4279
Sugarbayar Dorj <dorjsugaraaz@gmail.com>
2022-11-22 09:56:30 +0800

    created GR1100

---
+++ src/container/contents/gr/gr1100/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { DetailHeader, DetailWrapper, Wrapper } from "../style";
+import DataGridFooter from "components/dataGridFooter/dataGridFooter";
+import Grid from "./grid";
+function GR1100({
+  depthFullName,
+  menuId,
+}: {
+  depthFullName: string;
+  menuId: string;
+}) {
+  const [data, setData] = useState([]);
+  return (
+    <>
+      <DetailHeader>
+        <p>{depthFullName}</p>
+      </DetailHeader>
+      <Wrapper>
+        {/* <Grid /> */}
+        <DetailWrapper></DetailWrapper>
+      </Wrapper>
+      <DataGridFooter dataLength={data?.length > 0 ? data.length : 0} />
+    </>
+  );
+}
+
+export default GR1100;
