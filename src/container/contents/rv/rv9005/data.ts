06f01fb401568a2301c43ab16ea953f4962bfb9d
dido <tuguldur.3003@gmail.com>
2023-01-30 13:23:05 +0800

    rv9005

---
+++ src/container/contents/rv/rv9005/data.ts
@@ -0,0 +1,270 @@
+import { ValueType } from "realgrid";
+
+export const fields = [
+  {
+    fieldName: "cuCno",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "areaCode",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjCuCode",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "viewCuName",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjDate",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjJungum",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjGum",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjJankg",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjGage",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gJDanga",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjKumack",
+    dataType: ValueType.TEXT,
+  },
+
+  {
+    fieldName: "maintCost",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjDc",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjPerkum",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjMisu",
+    dataType: ValueType.TEXT,
+  },
+
+  {
+    fieldName: "gjDangKum",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjTotal",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjMisujan",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjSldate",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "gjJiroDate",
+    dataType: ValueType.TEXT,
+  },
+];
+
+export const columns = [
+  {
+    name: "cuCno",
+    fieldName: "cuCno",
+    type: "data",
+    width: "20",
+    header: {
+      text: "순번",
+    },
+  },
+  {
+    name: "areaCode",
+    fieldName: "areaCode",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjCuCode",
+    fieldName: "gjCuCode",
+    type: "data",
+    width: "20",
+    header: {
+      text: "코드",
+    },
+  },
+  {
+    name: "viewCuName",
+    fieldName: "viewCuName",
+    type: "data",
+    width: "40",
+    header: {
+      text: "거래처명",
+    },
+  },
+  {
+    name: "gjDate",
+    fieldName: "gjDate",
+    type: "data",
+    width: "20",
+    header: {
+      text: "검침일자",
+    },
+  },
+  {
+    name: "gjJungum",
+    fieldName: "gjJungum",
+    type: "data",
+    width: "20",
+    header: {
+      text: "전월",
+    },
+  },
+  {
+    name: "gjGum",
+    fieldName: "gjGum",
+    type: "data",
+    width: "20",
+    header: {
+      text: "당월",
+    },
+  },
+  {
+    name: "gjJankg",
+    fieldName: "gjJankg",
+    type: "data",
+    width: "20",
+    header: {
+      text: "사용량",
+    },
+  },
+  {
+    name: "gjGage",
+    fieldName: "gjGage",
+    type: "data",
+    width: "20",
+    header: {
+      text: "단가",
+    },
+  },
+  {
+    name: "gJDanga",
+    fieldName: "gJDanga",
+    type: "data",
+    width: "20",
+    header: {
+      text: "단가",
+    },
+  },
+  {
+    name: "gjKumack",
+    fieldName: "gjKumack",
+    type: "data",
+    width: "20",
+    header: {
+      text: "사용금액",
+    },
+  },
+
+  {
+    name: "maintCost",
+    fieldName: "maintCost",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjDc",
+    fieldName: "gjDc",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjPerkum",
+    fieldName: "gjPerkum",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjMisu",
+    fieldName: "gjMisu",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjDangKum",
+    fieldName: "gjDangKum",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjTotal",
+    fieldName: "gjTotal",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjMisujan",
+    fieldName: "gjMisujan",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjSldate",
+    fieldName: "gjSldate",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+  {
+    name: "gjJiroDate",
+    fieldName: "gjJiroDate",
+    type: "data",
+    width: "20",
+    header: {
+      text: "영업소",
+    },
+  },
+];
