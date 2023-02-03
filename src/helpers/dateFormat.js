2efd2e6289c971121a1a79ec1ae0ea724c086971
otgonjargal2019 <50820049+otgonjargal2019@users.noreply.github.com>
2023-02-03 17:03:26 +0800

    rv1100

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,5 +1,5 @@
 export function formatDate(date) {
-  date = date.replaceAll("-", "");
+  // date = date.replaceAll("-", "");
   const year = date?.substring(0, 4);
   const month = date?.substring(4, 6);
   const day = date?.substring(6, 8);
