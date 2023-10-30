e0a52bcbed537f937f530013c2144a4c01c67766
pjkkpjkkpjkk <pjkkpjkk@gmail.com>
2023-10-30 15:13:34 +0900

    zasvar

--- src/container/contents/ar/ar1100/tabs/tab6/modal1/data.ts
+++ src/container/contents/ar/ar1100/tabs/tab6/modal1/data.ts
@@ -2,137 +2,42 @@ import { ValueType } from "realgrid";
 
 export const fields = [
   {
-    fieldName: "gjGumymsno",
+    fieldName: "id",
+    dataType: ValueType.NUMBER,
+  },
+  {
+    fieldName: "name",
     dataType: ValueType.TEXT,
   },
   {
-    fieldName: "gjDate",
+    fieldName: "icon",
     dataType: ValueType.TEXT,
   },
   {
-    fieldName: "gjJungum",
-    dataType: ValueType.TEXT,
-  },
-  {
-    fieldName: "gjGum",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gjGage",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gjDangkum",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gjMisu",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gjPerkum",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gjTotal",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gjJirodate",
-    dataType: ValueType.TEXT,
-  },
-  {
-    fieldName: "gjMisujan",
-    dataType: ValueType.NUMBER,
-  },
-  {
-    fieldName: "gsSwName",
+    fieldName: "treeId",
     dataType: ValueType.TEXT,
   },
 ];
 
 export const columns = [
   {
-    name: "bglBpCode",
-    fieldName: "bglBpCode",
-    width: "30",
-    type: "data",
-    header: {
-      text: "코드",
-    },
-    editable: false,
-    footer: {
-      valueCallback: function (grid: any) {
-        return grid.getSummary("areaCode", "count");
-      },
-    },
-  },
-  {
-    name: "bglBpName",
-    fieldName: "bglBpName",
-    button: "action",
-    buttonVisibility: "always",
-    width: "90",
-    styleName: "rg-left-column",
-    type: "data",
-    header: {
-      text: "품명",
-    },
-    editable: false,
-  },
-  {
-    name: "bglBpType",
-    fieldName: "bglBpType",
-    width: "50",
-    type: "data",
-    styleName: "rg-right-column",
-    numberFormat: "#",
-    header: {
-      text: "규격",
-    },
-  },
-  {
-    name: "bglQty",
-    fieldName: "bglQty",
-    width: "50",
-    type: "data",
-    styleName: "rg-right-column",
-    numberFormat: "#",
-    header: {
-      text: "수량",
-    },
-  },
-  {
-    name: "bglDanga",
-    fieldName: "bglDanga",
-    width: "60",
-    type: "data",
-    styleName: "rg-right-column",
-    numberFormat: "#",
-    header: {
-      text: "단가",
-    },
-  },
-  {
-    name: "bglKumack",
-    fieldName: "bglKumack",
+    name: "id",
+    fieldName: "id",
     width: "80",
-    type: "data",
-    styleName: "rg-right-column",
-    numberFormat: "#",
-    header: {
-      text: "금액",
-    },
   },
   {
-    name: "bglBigo",
-    fieldName: "bglBigo",
-    width: "150",
-    type: "data",
-    styleName: "rg-right-column",
-    numberFormat: "#",
-    header: {
-      text: "비고",
-    },
+    name: "name",
+    fieldName: "name",
+    width: "120",
+  },
+  {
+    name: "icon",
+    fieldName: "icon",
+    width: "250",
+  },
+  {
+    name: "treeId",
+    fieldName: "treeId",
+    width: "80",
   },
 ];
